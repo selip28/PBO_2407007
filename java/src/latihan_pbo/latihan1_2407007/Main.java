f7525e476b2674cf541bb5bbf8e91892b33ef415
selip28 <abdulselip28@gmail.com>
2026-02-05 09:35:57 +0700

    pratikum 1 2407007

---
+++ java/src/latihan_pbo/latihan1_2407007/Main.java
@@ -0,0 +1,10 @@
+package latihan_pbo.latihan1_2407007;
+
+public class Main {
+    
+    public static void main(String[] args) {
+        System.out.println("pratikum 1");
+        System.out.println("nama : Abdul Adzim");
+        System.out.println("NIM : 2407007");    
+    }
+}
